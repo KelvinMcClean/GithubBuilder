28ebc7303ad9981357314bb1e9e8bf87e7d4f43f
KelvinMcClean <kmcclean11@qub.ac.uk>
2019-02-07 16:04:47 +0000

    Auth + Starting Projects
    Added basic authentication (using OAuth). Might rework once I get a chance.
    
    Started scraping some beginner projects. Caps at 30 currently. Need to rework metrics, and figure out pagination.

--- MainDirectory/Test.py
+++ MainDirectory/Test.py
@@ -2,26 +2,39 @@
 database = "D:/Experiment/Data.db"
 
 db_loc = "Database location: " + database
+auth_loc = "D:/Experiment/auth/auth.txt"
+user_loc = "D:/Experiment/auth/username.txt"
+f = open(auth_loc, "r")
+key_auth = f.read()
+f.close()
+f = open(user_loc, "r")
+key_user = f.read()
+f.close()
 sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
                                        id integer PRIMARY KEY,
                                        username text NOT NULL,
                                        year_created integer,
                                        hireable integer,
                                        public_repos integer,
+                                       owned_repos integer,
                                        followers integer,
                                        following integer,
-                                       location text
+                                       location text,
+                                       github_id integer NOT NULL
                                    ); """
 
-sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+sql_create_projects_table = """CREATE TABLE IF NOT EXISTS projects (
                                     id integer PRIMARY KEY,
+                                    owner_id integer NOT NULL,
                                     name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                    stargazer_count integer,
+                                    watchers_count integer,
+                                    forks_count integer,
+                                    year_created integer,
+                                    year_updated integer,
+                                    programming_language text,
+                                    github_id integer NOT NULL
+                                    
                                 );"""
 
 
--- MainDirectory/__init__.py
+++ MainDirectory/__init__.py
@@ -2,13 +2,13 @@ from GithubBuilder.MainDirectory.Test import *
 import sqlite3
 from sqlite3 import Error
 import requests
+import json
 
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
         return conn
     except Error as e:
         print(e)
@@ -28,28 +28,91 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
+def insert_into_people(conn, person):
+    try:
+        sql = '''INSERT INTO users 
+                    (username, year_created, hireable, public_repos, owned_repos, followers, following, github_id) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
+        c = conn.cursor()
+        c.execute(sql, person)
+        conn.commit()
+
+        return c.lastrowid
+    except Error as e:
+        print(e)
+
+
+def insert_into_projects(conn, project):
+    try:
+        sql = '''INSERT INTO projects 
+                    (owner_id, name, stargazer_count, watchers_count, forks_count, year_created, year_updated, 
+                    programming_language, github_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+        c = conn.cursor()
+        c.execute(sql, project)
+        conn.commit()
+
+        return c.lastrowid
+    except Error as e:
+        print(e)
+
 def main():
     print("Load up file for results...")
     print(db_loc)
 
-    #conn = create_connection(database)
-    #response = requests.get('https://api.github.com/search/repositories?q=stars%3A%3E0&sort=stars')
+    conn = create_connection(database)
 
-    #print('Number of projects: {0}'.format(response.json()['total_count']))
+    if conn is not None:
+        create_table(conn, sql_create_users_table)
+        create_table(conn, sql_create_projects_table)
 
-    response = requests.get('https://api.github.com/users/reergymerej')
-    print('Number of repos: {0}'.format(response.json()['public_repos']))
-    person = response.json()['login']
-    response = requests.get('https://api.github.com/users/{0}/repos' .format(person))
-    print(response.json()[0]['id'])
-    """if conn is not None:
+        response = requests.get('https://api.github.com/users/reergymerej', auth=(key_user, key_auth))
+        username = response.json()['login']
+        year_created = response.json()['created_at'][:4]
+        hireable = "0"
+        if response.json()['hireable']:
+            hireable = "1"
+        public_repos = str(response.json()['public_repos'])
+
+        followers = str(response.json()['followers'])
+        following = str(response.json()['following'])
+        github_id = str(response.json()['id'])
+        repos_list = requests.get('https://api.github.com/users/{0}/repos'.format(username), auth=(key_user, key_auth))
+        owned_repos = len(repos_list.json())
+
+        person = (username, year_created, hireable, public_repos, owned_repos, followers, following, github_id)
+
+        person_id = insert_into_people(conn, person)
+        print(person_id)
+
+        for i in range(len(repos_list.json())):
+            repo = repos_list.json()[i]
+            url = repo['url']
+            project_data = requests.get(url, auth=(key_user, key_auth))
+            name = project_data.json()['name']
+            stargazer_count = project_data.json()['stargazers_count']
+            watchers_count = project_data.json()['watchers_count']
+            forks_count = project_data.json()['forks']
+            year_created = project_data.json()['created_at'][:4]
+            year_updated = project_data.json()['updated_at'][:4]
+            programming_language = project_data.json()['language']
+            github_id = project_data.json()['id']
+            project = (person_id,name, stargazer_count, watchers_count, forks_count, year_created, year_updated,
+                       programming_language, github_id)
+            insert_into_projects(conn, project)
+
+        conn.close()
+    """print('Number of projects: {0}'.format(response.json()['total_count']))
+    #print('Number of repos: {0}'.format(response.json()['public_repos']))
+    #print(response.json()[0]['id'])
+    #print(len(response.json()))
+    if conn is not None:
         create_table(conn, sql_create_users_table)
 
         conn.close()
 
     else:
         print("Error! Cannot create database Connection")
-"""
+
     print("See where to start...")
 
     print("Load command to go to GH and get data")
@@ -57,6 +120,7 @@ def main():
     print("Count API calls")
 
     print("Process call and data received. Process.")
+"""
 
 
 if __name__ == "__main__":
