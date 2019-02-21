414eb8a22fc85293a430e569f6c99c773558f281
KelvinMcClean <kmcclean11@qub.ac.uk>
2019-02-21 16:04:06 +0000

    Changes LOC to size

--- Database/Project.py
+++ Database/Project.py
@@ -1,11 +1,11 @@
 class Project:
 
-    def __init__(self, p_id, name, owner, space, LOC, contributor_count, contributors, dates, issues):
+    def __init__(self, p_id, name, owner, space, size, contributor_count, contributors, dates, issues):
         self.p_id = p_id
         self.name = name
         self.owner = owner
         self.space = space
-        self.LOC = LOC
+        self.size = size
         self.contributor_count = contributor_count
         self.contributors = contributors
         self.dates = dates
