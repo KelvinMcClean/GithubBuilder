dcb5b539920f6052fd7caa87e1974d5921d4ec0f
KelvinMcClean <kmcclean11@qub.ac.uk>
2019-03-01 14:34:45 +0000

    Query Info for GHTorrent
    Added more queries to get Data from GHT.

--- Database/GHTorrent/Connection.py
+++ Database/GHTorrent/Connection.py
@@ -11,9 +11,30 @@ def get_ghtorrent_user(login):
     return cur.fetchone()
 
 
-def get_ghtorrent_is_in_org(ghtorrent_id):
+def get_ghtorrent_user_orgs(ghtorrent_id):
     response = cur.execute("SELECT org_id, created_at from organization_members WHERE user_id='{0}'".format(ghtorrent_id))
     if response == 0:
+        return response
+    else:
+        return cur.fetchall()
+
+
+def get_ghtorrent_user_projects_from_login(login):
+    response = cur.execute("SELECT p.id, p.url, p.name, p.language, p.created_at, p.updated_at, p.deleted "
+                           "FROM projects as p "
+                           "INNER JOIN users ON users.id=p.owner_id "
+                           "WHERE users.login='{0}' AND p.forked_from IS NULL;".format(login))
+    if response ==0:
+        return response
+    else:
+        return cur.fetchall()
+
+
+def get_ghtorrent_user_projects(ghtorrent_id):
+    response = cur.execute("SELECT p.id, p.url, p.name, p.language, p.created_at, p.updated_at, p.deleted "
+                           "FROM projects as p"
+                           "WHERE p.owner_id='{0}' AND p.forked_from IS NULL;".format(ghtorrent_id))
+    if response ==0:
         return response
     else:
         return cur.fetchall()
